f5114e8ff2874eadaf6350fc8cc826b7f51e9d39
Toben <zxcv1867@naver.com>
2023-04-21 19:47:42 +0900

    str을 씹고 뜯는 함수 만들기

--- Cdrill/main.c
+++ Cdrill/main.c
@@ -1,13 +1,26 @@
-// 문자열 입력
+// copy_str 사용 예제
 #include <stdio.h>
+int copy_str(char *src, char *dest);
 int main() {
-	char words[30];
+	char str1[] = "hello";
+	char str2[] = "hi";
 	
-	printf("30 자 이내의 문자열을 입력해주세요! : ");
-	scanf("%s", words);
+	printf("복사 이전 : %s \n", str1);
 	
-	printf("문자열 : %s \n", words);
+	copy_str(str1, str2);
+	
+	printf("복사 이후 : %s \n", str1);
 	
 	return 0;
 }
-
+int copy_str(char *dest, char *src) {
+	while (*src) {
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	
+	*dest = '\0';
+	
+	return 1;
+}
